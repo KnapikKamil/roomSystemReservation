76905656acb924b9bf2b1e625c48a6a612870ca3
Nowe Kamil Knapik <MrKamilKnapik@gmail.com>
2023-11-05 23:35:28 +0100

    GUI room continue

--- src/main/java/org/exampleHotel/ui/gui/AdNewRoomScene.java
+++ src/main/java/org/exampleHotel/ui/gui/AdNewRoomScene.java
@@ -1,7 +1,12 @@
 package org.exampleHotel.ui.gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -18,21 +23,41 @@ public class AdNewRoomScene {
     private final List<ComboBox<String>> comboBoxes = new ArrayList<>();
     private final RoomService roomService = ObjectPool.getRoomService();
     public AdNewRoomScene(Stage stg, TableView<RoomDTO> tableView){
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(8);
+
         Label roomNumberLabel = new Label("Numer pokoju:");
         TextField roomNumberField = new TextField();
-        HBox roomNumber = new HBox(roomNumberLabel, roomNumberField);
+
+        gridPane.add(roomNumberLabel, 0, 0);
+        gridPane.add(roomNumberField, 1, 0);
+
 
         Label bedTypeLabel = new Label("Typy łóżek:");
 
-        Button adNewBedButton = new Button("Dodaj kolejne łóżko");
+        Button addNewBedButton = new Button();
 
-        HBox bedType = new HBox(bedTypeLabel, adNewBedButton);
+        Image icon = new Image(getClass().getClassLoader().getResourceAsStream("add.png"));
+
+        ImageView imageView = new ImageView(icon);
+
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(20);
+        
+        addNewBedButton.setGraphic(imageView);
+
+        addNewBedButton.setPadding(Insets.EMPTY);
+
+        gridPane.add(bedTypeLabel, 0, 1);
+        gridPane.add(addNewBedButton, 1, 1);
 
 
 
-        VBox bedsVerticalLayout = new VBox(bedType, getComboBox());
+        VBox bedsVerticalLayout = new VBox(getComboBox());
 
-        adNewBedButton.setOnAction(actionEvent -> {
+        addNewBedButton.setOnAction(actionEvent -> {
         bedsVerticalLayout.getChildren().add(getComboBox());
         });
         Button addNewRoomButton = new Button("Dodaj nowy pokój");
@@ -44,21 +69,27 @@ public class AdNewRoomScene {
                 bedTypes.add(comboBoxes.getValue());
             });
            this.roomService.createNewRoom(newRoomNumber, bedTypes);
+
+
            tableView.getItems().clear();
            List<RoomDTO> allAsDTO = roomService.getAllAsDTO();
            tableView.getItems().addAll(allAsDTO);
+            stg.close();
 
-           stg.close();
         });
 
-        VBox vmainFormLayout = new VBox(roomNumber, bedsVerticalLayout, addNewRoomButton);
+        addNewRoomButton.setPadding(new Insets(4, 4, 4, 4));
 
-        this.mainScene = new Scene(vmainFormLayout,1280,640);
+        gridPane.add(bedsVerticalLayout, 1, 2);
+        gridPane.add(addNewRoomButton, 1, 3);
+
+
+        this.mainScene = new Scene(gridPane,1280,640);
     }
 
     private ComboBox <String> getComboBox() {
         ComboBox <String> bedTypeField = new ComboBox<>();
-        bedTypeField.getItems().addAll("Pojedyńcze", "podwójne", "Królewskie");
+        bedTypeField.getItems().addAll("Pojedyńcze", "Podwójne", "Królewskie");
         bedTypeField.setValue("Pojedyńcze");
         this.comboBoxes.add(bedTypeField);
         return bedTypeField;
